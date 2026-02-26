df7442f52af54c5f162890abf1dd6c019e1d3143
Kunzhen Wu <wkzmagician@gmail.com>
2026-02-26 18:04:20 +0800

    修复bug: 涅奥的祝福没有效果

--- relics/global_relics/special.py
+++ relics/global_relics/special.py
@@ -1,9 +1,10 @@
 """
 Special Relics
-Special key items for the game.
+Special key items for game.
 """
 from typing import List
 from actions.base import Action
+from actions.display import DisplayTextAction
 from relics.base import Relic
 from utils.types import RarityType
 from utils.registry import register
@@ -49,5 +50,29 @@ class NeowsLament(Relic):
         super().__init__()
         self.rarity = RarityType.SPECIAL
         self.stacks = 3
+        
+    def on_combat_start(self, player, entities) -> List[Action]:
+        """在前三场战斗中设置所有敌人 HP=1"""
+        from engine.game_state import game_state
+        
+        # 检查是否在前三场战斗中
+        if self.stacks > 0:
+            actions = []
+            
+            # 设置所有敌人的 HP 和最大 HP 为 1
+            for enemy in entities:
+                if hasattr(enemy, 'hp'):
+                    enemy.hp = 1
+                    
+            # 添加显示消息
+            actions.append(DisplayTextAction(
+                text_key="relics.neows_lament.effect"
+            ))
+            
+            self.stacks -= 1
+            
+            return actions
+        
+        return []
     
     # This relic provides benefits when certain conditions are met
